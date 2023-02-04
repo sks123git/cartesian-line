279d328c236c30fe91f28b0521f26d789be9ecff
subzero <shubham8111994@gmail.com>
2023-02-05 01:52:16 +0530

    added feature to check equality between 2 lines

--- src/CartesianLineLength.java
+++ src/CartesianLineLength.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 // Program to find length between two cartesian points.
 public class CartesianLineLength {
 
-    public static int line1,line2;
     public int lengthCalculate(int x1,int y1, int x2, int y2){ // Calculate length and return
         int length = (int)Math.pow((x2-x1),2) + (int)Math.pow((y2-y1),2);
         return (int)Math.sqrt(length);
@@ -26,10 +25,12 @@ public class CartesianLineLength {
         System.out.println("Kindly enter second coordinates x4 and y4: ");
         x4 = input.nextInt();
         y4 = input.nextInt();
-        line1 =  length.lengthCalculate(x1,y1,x2,y2);
-        line2 =  length.lengthCalculate(x3,y3,x4,y4);
+
+
+        Integer line1 =  length.lengthCalculate(x1,y1,x2,y2);
+        Integer line2 =  length.lengthCalculate(x3,y3,x4,y4);
         // Check equality of lines
-        if(line1==line2){
+        if(line1.equals(line2)) {
             System.out.println("Both lines are equal");
         } else {
             System.out.println("Both lines are not equal");
