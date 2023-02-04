d61eade59c94ee69789b52cee792de9bd1e86ffa
subzero <shubham8111994@gmail.com>
2023-02-04 23:56:48 +0530

    added code to calculate length between two cartesian points

--- src/CartesianLineLength.java
+++ src/CartesianLineLength.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+// Program to find length between two cartesian points.
 public class CartesianLineLength {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    public int lengthCalculate(int x1,int y1, int x2, int y2){ // Calculate length and return
+        int length = (int)Math.pow((x2-x1),2) + (int)Math.pow((y2-y1),2);
+        return (int)Math.sqrt(length);
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        CartesianLineLength length = new CartesianLineLength();
+        int x1,y1,x2,y2;
+        System.out.println("Kindly enter first coordinates x1 and y1: ");
+        x1 = input.nextInt();
+        y1 = input.nextInt();
+        System.out.println("Kindly enter second coordinates x2 and y2: ");
+        x2 = input.nextInt();
+        y2 = input.nextInt();
+
+        System.out.println("length of line: " + length.lengthCalculate(x1,y1,x2,y2));
+
     }
 }
